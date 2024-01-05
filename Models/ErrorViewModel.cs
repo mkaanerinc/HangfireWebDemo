98fc226f1dcc5c99d4e641feb5f206f157b299c7
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-01-05 11:49:51 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HangfireWebDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
